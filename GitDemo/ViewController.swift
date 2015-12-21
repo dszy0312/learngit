e924189378d5267a3acee818ca3d1d1d4bf7a3fd
weihui <git@weihui.com>
2015-12-21 17:28:50 +0800

    一个重要的修改

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var  a = 5
+        print("\(a)")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
